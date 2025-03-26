a336d4a555e25e028ff456a2d7ba066d4801735c
7Priyanshu9 <priyanshupaul32@gmail.com>
2025-03-27 00:50:45 +0530

    interview card upated

--- components/DisplayTech.tsx
+++ components/DisplayTech.tsx
@@ -1,8 +1,16 @@
+import { getTechLogos } from '@/constants/utils'
 import React from 'react'
+import Image from 'next/image';
 
-const DisplayTech = ({techstack}:TechIconProps) => {
+const DisplayTech = async ({techStack}:TechIconProps) => {
+    const techIcons = await getTechLogos(techStack);
   return (
-    <div>{techstack}</div>
+    <div className='flex flex-row '>{techIcons.slice(0,3).map(({tech,url},index)=>(
+        <div key={tech} className=' relative group bg-dark-100 rounded-full p-2 flex-center '>
+            <span className='tech-tooltip' >{tech}</span>
+            <Image src={url} alt={tech} width={100} height={100} className='size-5'/>
+        </div>
+    ))}</div>
   )
 }
 
--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -9,59 +9,65 @@ import DisplayTech from './DisplayTech';
 
 // import { getFeedbackByInterviewId } from '@/lib/actions/feedback.actions';
 
-const InterviewCard = async({id,
+const InterviewCard = async ({ id,
     userId,
     role,
     type,
     techstack,
     level,
-    timeCreated}:InterviewCardProps) => {
+    timeCreated }: InterviewCardProps) => {
 
     const feedback = null as Feedback | null;
     // if teh user have mixed technical  with beahviuoral then do this 
 
-    const normalise = /mix/gi.test(type)? 'Mixed':type;
+    const normalise = /mix/gi.test(type) ? 'Mixed' : type;
 
     const formattedDate = dayjs(feedback?.timeCreated || timeCreated || Date.now()).format('DD/MM/YYYY');
 
+    const badge = {
+        Behavioral: 'ng-light-400',
+        Mixed: "bg-light-600",
+        Technical: "bg-light-800",
+    }
+    [normalise] || "bg-light-600";
 
-  return (
-    <div className='card-border w-[410px] max-sm:w-full min-h-96'>
-        <div className='card-interview'>
-            <div>
-                <div className='absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600'>
-                    <p className='badge-text'>{normalise}</p>
-                </div>
-                <Image src={getRandomInterviewCover()} alt='interview cover' width={90} height={90} className='rounded-full object-fit size-[90px]'/>
+    return (
+        <div className='card-border w-[410px] max-sm:w-full min-h-96'>
+            <div className='card-interview'>
+                <div>
+                    <div className='absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600'>
+                        <p className='badge-text'>{normalise}</p>
+                    </div>
+                    <Image src={getRandomInterviewCover()} alt='interview cover' width={90} height={90} className='rounded-full object-fit size-[90px]' />
 
-                <h3 className='mt-5 capitalize'>
-                    {role} Interview
-                </h3>
+                    <h3 className='mt-5 capitalize'>
+                        {role} Interview
+                    </h3>
 
-                <div className='flex flex-row gap-5 mt-3'>
-                    <div className='flex flex-row gap-2'>
-                        <Image src='/calendar.svg' alt='calender' width={20} height={20}/>
-                        <p>{formattedDate}</p>
+                    <div className='flex flex-row gap-5 mt-3'>
+                        <div className='flex flex-row gap-2'>
+                            <Image src='/calendar.svg' alt='calender' width={20} height={20} />
+                            <p>{formattedDate}</p>
+                        </div>
+
+                        <div className='flex flex-row gap-2 items-center'>
+                            <Image src='/star.svg' alt='star' height={22} width={22} />
+                            <p>{feedback?.totalScore || '---'}/100</p>
+                        </div>
                     </div>
 
-                    <div className='flex flex-row gap-2 items-center'>
-                        <Image src='/star.svg' alt='star' height={22} width={22}/>
-                        <p>{feedback?.totalScore || '---'}/100</p>
-                    </div>
+                    <p className='line-clamp-2 mt-5'>{feedback?.finalAssessment || 'You have not taken this interview yet'}</p>
                 </div>
 
-                <p className='line-clamp-2 mt-5'>{feedback?.finalAssessment || 'You have not taken this interview yet'}</p>
-            </div>
-
-            <div className='flex flex-row justify-between'    >
-                <DisplayTech techStack={techstack}/>                <Button className='btn-primary'>
-                    <Link href={feedback? `/interview/${id}/feedback`:`/interview/${id}`}>
-                    {feedback?'Check Feedback':'View Interview'}</Link>
-                </Button>
+                <div className='flex flex-row justify-between'    >
+                    <DisplayTech techStack={techstack} />                <Button className='btn-primary'>
+                        <Link href={feedback ? `/interview/${id}/feedback` : `/interview/${id}`}>
+                            {feedback ? 'Check Feedback' : 'View Interview'}</Link>
+                    </Button>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 // here 
 
